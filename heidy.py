c93b3a2cf74f989b02b70a9953c097ecd02db547
heidyibarra <Heidyibarra88@gmail.com>
2017-03-02 20:37:35 -0500

    Add files via upload
    http://www.codeskulptor.org/#user42_F2gSg405hSoYjVP_2.py

---
+++ heidy.py
@@ -0,0 +1,13 @@
+print("Primer Punto")
+for p in range (20,0,-1):
+    print(p)
+ 
+
+print("Segundo Punto")
+
+cadena="Cansadas cargadas rapadas marchaban las chavas, calladas calmadas bandadas de gatas las ratas cazaban, las ranas cantaban llamaban saltaban y al saltar sanaban de su mal astral"
+contador=0
+for i in cadena :
+    if (i=="a"):
+        contador=contador+1
+print(contador)
